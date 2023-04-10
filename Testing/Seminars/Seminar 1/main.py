cf1e415ec3f05a8a588adf3b52aaf83cc568aaed
Evgeniy Osadchiy <osadchiy.evgeniy88@gmail.com>
2023-04-10 09:26:37 +0300

    Логические операции

--- Testing/Seminars/Seminar 1/main.py
+++ Testing/Seminars/Seminar 1/main.py
@@ -1,1 +1,103 @@
-print (5)
+"""
+a = 5 
+b = 5.89
+c = 'hello'
+
+print(a, ' - ', b, ' - ',c)
+"""
+
+"""
+a = 5 
+b = 5.89
+c = 'hello'
+
+print(f"{a} - {b} - {c}")
+"""
+
+"""
+a = 5 
+b = 5.89
+c = 'hello'
+
+print("{} - {} - {}".format(a,b,c))
+"""
+
+"""
+print('Введите первую строку')
+a = input()
+print(a)
+"""
+
+"""
+print('Введите первое число: ')
+a = input()
+print(a)
+"""
+
+"""
+print('Введите первое число: ')
+a = input()
+
+b = input('Введите второе число: ')
+"""
+
+"""
+print('Введите первое число: ')
+a = input()
+b = input('Введите второе число: ')
+
+print(a, ' + ', b, ' = ', a+b)
+"""
+
+"""
+c = 5.89
+print(c)
+print(type(c))
+c = int(c)
+print(c)
+print(type(c))
+"""
+
+"""
+c = 5.89
+print(c)
+print(type(c))
+c = str(c)
+print(c + '89')
+print(type(c))
+"""
+
+"""
+c = 1
+print(c)
+print(type(c))
+c = bool(c)
+print(c)
+print(type(c))
+"""
+
+
+"""
+print('Введите первое число: ')
+a = int (input())
+b = int (input('Введите второе число: '))
+
+print(a, ' + ', b, ' = ', a+b)
+"""
+
+
+"""
+a = 5.89956
+b = 6.556551
+print(round(a*b, 3))
+"""
+
+
+iter = 2
+iter += 3 #iter = iter + 3
+iter -= 4 #iter = iter - 4
+iter *= 5 #iter = iter * 5
+iter /= 5 #iter = iter / 5
+iter //= 5 #iter = iter // 5
+iter %= 5 #iter = iter % 5
+iter **= 5 #iter = iter ** 5
