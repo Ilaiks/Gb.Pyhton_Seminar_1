4880a325e3ba53bf88642f0ee8863873f5e86a80
Evgeniy Osadchiy <osadchiy.evgeniy88@gmail.com>
2023-04-28 19:41:01 +0300

    task4

--- Testing/Seminars Work/Seminar 1/task2.py
+++ Testing/Seminars Work/Seminar 1/task2.py
@@ -7,3 +7,16 @@
 100 -> 1 (1 + 0 + 0)
 """
 
+a = 123
+print(a//100 + a//10%10 + a%10)
+
+# или
+
+num = input('Введите 3-х значное число: ')
+res = 0
+if len(num) == 3:
+    for i in num:
+        res += int(i)
+    print(res)
+else:
+    print('Вы ввели не 3-х значное число')
